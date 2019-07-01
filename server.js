43a9056670207616a68d09dee4c55ae89a70b21d
Dexter Lewis <dexter.lewis@gmail.com>
2019-06-30 21:47:14 -0400

    wrote api to post new game

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const server = express();
 server.use(express.json());
 
 let games = [
-    { title: 'Pacman', genre: 'Arcade', releaseYear: 1980 },
-    { title: 'Black Ops 4', genre: 'FPS', releaseYear: 2019 },
-    { title: 'Last Of Us', genre: 'Survival Horror', releaseYear: 2013 }
-  ];
+  { title: 'Pacman', genre: 'Arcade', releaseYear: 1980 },
+  { title: 'Black Ops 4', genre: 'FPS', releaseYear: 2019 },
+  { title: 'Last Of Us', genre: 'Survival Horror', releaseYear: 2013 }
+];
 
 server.get('/', (req, res) => {
-    res.send('Up and running');
-  });
+  res.send('Up and running');
+});
 
-  server.get('/games', (req, res) => {
+server.get('/games', (req, res) => {
   res.status(200).json(games);
 });
 
-  module.exports = { server, games}
+server.post('/games', (req, res) => {
+  const game = req.body;
+  games = [...games, game];
+  res.status(201).json(games[games.length - 1]);
+});
+
+module.exports = { server, games };
