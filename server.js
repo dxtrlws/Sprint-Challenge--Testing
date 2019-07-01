58da77be69123d861b9ffd8d784020eb7909f3f0
Dexter Lewis <dexter.lewis@gmail.com>
2019-06-30 21:20:31 -0400

    setup array database to hold  a list of games

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const server = express();
 
 server.use(express.json());
 
+let games = [
+    { title: 'Pacman', genre: 'Arcade', releaseYear: 1980 },
+    { title: 'Black Ops 4', genre: 'FPS', releaseYear: 2019 },
+    { title: 'Last Of Us', genre: 'Survival Horror', releaseYear: 2013 }
+  ];
+
 server.get('/', (req, res) => {
     res.send('Up and running');
   });
